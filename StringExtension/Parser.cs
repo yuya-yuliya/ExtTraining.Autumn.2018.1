8f56ecfa8338d0161e4c60e96aaa3ed54e2e026f
AnzhelikaKravchuk <https://github.com/AnzhelikaKravchuk/>
2018-10-28 19:20:50 +0300

    Add task 2

---
+++ StringExtension/Parser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringExtension
+{
+    public static class Parser
+    {
+        public static int ToDecimal(this string str, int @base)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
